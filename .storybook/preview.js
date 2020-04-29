4fb51fe44ed3abd03a59063cfd5ebdf72ad4ca27
Jacob Coughenour <jacob15545@yahoo.com>
2020-04-29 00:58:48 -0400

    add theme system

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,11 @@
+import '../src/index.css';
+
 import { addDecorator, addParameters } from '@storybook/react';
 
-import centered from '@storybook/addon-centered';
+import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import centered from '@storybook/addon-centered/react';
+import theme from '../src/theme';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 
@@ -10,7 +15,12 @@ addDecorator(withA11y);
 
 addParameters({
 	backgrounds: [
+		{ name: 'theme', value: theme.palette.background.default },
 		{ name: 'dark', value: '#1b1e2b' },
 		{ name: 'light', value: 'white' },
 	],
 });
+
+addDecorator((storyFn) => (
+	<ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+));
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
