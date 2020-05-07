0c48eecf04b7ffdafd8957cedb6d4986be47c046
jbofill10 <juan.bofill10@gmail.com>
2020-05-06 20:08:42 -0400

    Remove youtube api

--- backend/app/routes/clients/Clients.js
+++ backend/app/routes/clients/Clients.js
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 import Twitter from 'twitter';
-import { YoutubeDataAPI } from 'youtube-v3-api'
 
 const TwitterClient = new Twitter({
     consumer_key: String(process.env.TWITTER_API_KEY),
@@ -8,7 +7,4 @@ const TwitterClient = new Twitter({
     bearer_token: String(process.env.TWITTER_ACCESS_TOKEN)
 });
 
-const YoutubeClient = new YoutubeDataAPI(String(process.env.GOOGLE_API_KEY));
-
-
-export {TwitterClient, YoutubeClient}
+export { TwitterClient }
